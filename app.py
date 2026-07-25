bab93706b5b6c75f3ed375f9ab93bc136feba12b
Mohit srivatava <mohitsrivastava082@gmail.com>
2026-07-26 00:19:30 +0530

    Refactor app.py to improve PDF upload and processing

--- app.py
+++ app.py
@@ -59,27 +59,27 @@ if uploaded_file:
 
 
 
-if os.path.exists("chroma_db"):
+    if os.path.exists("chroma_db"):
 
-     embeddings = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
+       embeddings = HuggingFaceEmbeddings(
+             model_name="sentence-transformers/all-MiniLM-L6-v2"
+     )
 
-     vectorstore = Chroma(
-        persist_directory="chroma_db",
-        embedding_function=embeddings
-    )
+          vectorstore = Chroma(
+            persist_directory="chroma_db",
+             embedding_function=embeddings
+     )
 
-      retriever = vectorstore.as_retriever(
-        search_type="mmr",
-        search_kwargs={
-            "k":4,
-            "fetch_k":10,
-            "lambda_mult":0.5
-        }
-    )
+         retriever = vectorstore.as_retriever(
+            search_type="mmr",
+            search_kwargs={
+              "k":4,
+              "fetch_k":10,
+              "lambda_mult":0.5
+         }
+     )
 
-    llm = ChatMistralAI(model="mistral-small-2506")
+      llm = ChatMistralAI(model="mistral-small-2506")
 
     prompt = ChatPromptTemplate.from_messages(
         [
