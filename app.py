7ed543746b1489c8b0c8796f469ca3aa9371fa0a
Mohit srivatava <mohitsrivastava082@gmail.com>
2026-07-25 23:52:30 +0530

    Reformat app.py for improved readability

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ if os.path.exists("chroma_db"):
             model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
-    vectorstore = Chroma(
+      vectorstore = Chroma(
         persist_directory="chroma_db",
         embedding_function=embeddings
     )
